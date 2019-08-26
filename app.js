e74cb9503b964d0d436fd60953bb6345bef1de8c
Greg Jackson <gregoryljackson@gmail.com>
2019-08-26 12:01:38 -0400

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var expressMessages = require('express-messages');
+
+
+//Connect to DB
+mongoose.connect(config.database, {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Connected to MongoDB")
+});
+
+
+//Init app
+var app = express();
+
+//View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Set public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Set global errors variables
+app.locals.errors = null;
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Express Session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+//Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Set Route
+var pages = require('./routes/pages.js');
+var admin_pages = require('./routes/admin-page.js');
+
+app.use('/admin/pages', admin_pages);
+app.use('/', pages);
+
+
+//Start the server
+var port = 3000;
+app.listen(port, function(){
+  console.log('Server started on port ' + port);
+});
---
+++ routes/admin-page.js
@@ -0,0 +1,118 @@
+var express = require('express');
+var router = express.Router();
+
+//Get page Model
+var Page = require('../models/page');
+
+const { check, validationResult } = require('express-validator');
+
+
+/*
+* GET pages index
+*/
+
+router.get('/', function(req,res) {
+  Page.find({}).sort({sorting: 1}).exec(function(err, pages) {
+    res.render('admin/pages', {
+      pages: pages
+    });
+  });
+});
+
+
+/*
+* GET add page
+*/
+router.get('/add-page', function(req,res) {
+  var title = "";
+  var slug = "";
+  var content = "";
+
+  res.render('admin/add-page', {
+    title: title,
+    slug: slug,
+    content: content
+  });
+});
+
+/*
+* POST reorder pages
+*/
+router.post('/reorder-page', function(req,res) {
+  var ids = req.body['id[]'];
+  console.log(ids);
+
+  var count = 0;
+
+  for(var i = 0; i < ids.length; i ++) {
+    var id = ids[i];
+    count++;
+
+    Page.findById(id, function(err, page) {
+      page.sorting = count;
+      page.save(function(err) {
+        if(err) {
+          return console.log(err);
+        }
+      });
+    });
+  }
+});
+
+
+/*
+* POST add page
+*/
+router.post('/add-page', [
+  check('title', 'Title must have a value.').isLength({min:1}),
+  check('content', 'Content must have a value.').isLength({min:1})
+], (req, res) => {
+  var title = req.body.title;
+  var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+  if (slug == "")
+      slug = title.replace(/\s+/g, '-').toLowerCase();
+  var content = req.body.content;
+
+  var errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+      res.render('admin/add-page', {
+          errors: errors,
+          title: title,
+          slug: slug,
+          content: content
+      });
+  } else {
+    console.log("working");
+    Page.findOne({slug: slug}, function(err, page){
+      if(page) {
+        req.flash('danger', 'Page slug exists, choose another.');
+        res.render('admin/add-page', {
+          title: title,
+          slug: slug,
+          content: content
+        });
+      } else {
+        var page = new Page({
+          title: title,
+          slug: slug,
+          content, content,
+          sorting: 100
+        });
+        page.save(function(err) {
+          if(err) {
+            return console.log(err);
+          } else {
+            req.flash('success', 'Page added!');
+            res.redirect('/admin/pages');
+          }
+        });
+      }
+    });
+  }
+}
+);
+
+
+//Exports
+module.exports = router;
